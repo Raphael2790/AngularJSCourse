8f9f1ac87f6c8993d807e89772d9c8fd6e221bb1
Raphael2790 <rssshev@gmail.com>
2020-07-20 08:56:49 -0300

    Adicionar arquivos de projeto.

---
+++ CursoBaltaAngularJS/Aula4/app/app.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    const app = angular.module('app', ['ngAnimate', 'ngRoute'])
+
+    app.config(($routeProvider) => {
+        $routeProvider
+            .when('/',{
+                controller: 'HomeCtrl',
+                templateUrl: 'app/areas/home/views/index.html'
+            })
+            .otherwise({
+                controller: 'HomeCtrl',
+                templateUrl: '404.html'
+            })
+    })
+})();
